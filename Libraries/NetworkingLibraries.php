2e126cc5ad7b1b9f9e3b523a78687459ff5acacd
OotTheMonk <nathanandrecoder@gmail.com>
2024-03-23 10:56:58 -0500

    Relentless fix

--- Libraries/NetworkingLibraries.php
+++ Libraries/NetworkingLibraries.php
@@ -1630,7 +1630,7 @@ function PlayCardEffect($cardID, $from, $resourcesPaid, $target = "-", $addition
     }
     $EffectContext = $cardID;
     $playText = "";
-    if(!$chainClosed && (!DefinedTypesContains($cardID, "Event", $currentPlayer) || !SearchCurrentTurnEffects("3401690666", $currentPlayer))) $playText = PlayAbility($cardID, $from, $resourcesPaid, $target, $additionalCosts);
+    if(!$chainClosed && (!DefinedTypesContains($cardID, "Event", $currentPlayer) || !SearchCurrentTurnEffects("3401690666", $currentPlayer, remove:true))) $playText = PlayAbility($cardID, $from, $resourcesPaid, $target, $additionalCosts);
     if($from != "EQUIP" && $from != "PLAY") WriteLog("Resolving play ability of " . CardLink($cardID, $cardID) . ($playText != "" ? ": " : ".") . $playText);
     else if($from == "EQUIP" || $from == "PLAY") WriteLog("Resolving activated ability of " . CardLink($cardID, $cardID) . ($playText != "" ? ": " : ".") . $playText);
     if (!$openedChain) ResolveGoAgain($cardID, $currentPlayer, $from);
