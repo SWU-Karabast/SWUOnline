c26cca546e4d809ea2759378873e1ec74f1f28e7
OotTheMonk <nathanandrecoder@gmail.com>
2024-04-02 17:42:17 -0500

    A few steadfast battalion fixes

--- AllyAbilities.php
+++ AllyAbilities.php
@@ -526,13 +526,13 @@ function SpecificAllyAttackAbilities($attackID)
       DealArcane(1, 3, "PLAYCARD", "3646264648");
       break;
     case "6432884726"://Steadfast Battalion
-      if($hasLeader($mainPlayer)) {
+      if(HasLeader($mainPlayer)) {
         AddDecisionQueue("SETDQCONTEXT", $mainPlayer, "Choose a unit to give +2/+2");
         AddDecisionQueue("MULTIZONEINDICES", $mainPlayer, "MYALLY");
         AddDecisionQueue("MAYCHOOSEMULTIZONE", $mainPlayer, "<-", 1);
-        AddDecisionQueue("MZOP", $currentPlayer, "ADDHEALTH,2", 1);
+        AddDecisionQueue("MZOP", $mainPlayer, "ADDHEALTH,2", 1);
         AddDecisionQueue("MZOP", $mainPlayer, "GETUNIQUEID", 1);
-        AddDecisionQueue("ADDLIMITEDCURRENTEFFECT", $mainPlayer, "6432884726,HAND", 1);
+        AddDecisionQueue("ADDLIMITEDCURRENTEFFECT", $mainPlayer, "6432884726,PLAY", 1);
       }
       break;
     case "5e90bd91b0"://Han Solo
