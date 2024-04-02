13e30364f1688e248176848749b440cb0a6ad96c
OotTheMonk <nathanandrecoder@gmail.com>
2024-04-01 19:22:01 -0500

    Fix auto passing the round as soon as one player passes initiative

--- Libraries/NetworkingLibraries.php
+++ Libraries/NetworkingLibraries.php
@@ -713,7 +713,7 @@ function Passed(&$turn, $playerID)
 
 function PassInput($autopass = true)
 {
-  global $turn, $currentPlayer, $initiativeTaken;
+  global $turn, $currentPlayer, $initiativeTaken, $initiativePlayer;
   if($turn[0] == "END" || $turn[0] == "MAYMULTICHOOSETEXT" || $turn[0] == "MAYCHOOSECOMBATCHAIN" || $turn[0] == "MAYCHOOSEMULTIZONE" || $turn[0] == "MAYMULTICHOOSEAURAS" ||$turn[0] == "MAYMULTICHOOSEHAND" || $turn[0] == "MAYCHOOSEHAND" || $turn[0] == "MAYCHOOSEDISCARD" || $turn[0] == "MAYCHOOSEARSENAL" || $turn[0] == "MAYCHOOSEPERMANENT" || $turn[0] == "MAYCHOOSEDECK" || $turn[0] == "MAYCHOOSEMYSOUL" || $turn[0] == "MAYCHOOSETOP" || $turn[0] == "INSTANT" || $turn[0] == "OK") {
     ContinueDecisionQueue("PASS");
   } else {
@@ -722,7 +722,7 @@ function PassInput($autopass = true)
     if(Pass($turn, $currentPlayer, $currentPlayer)) {
       if($turn[0] == "M")
       {
-        if($initiativeTaken == 1) {
+        if($initiativeTaken == 1 && $initiativePlayer != $currentPlayer) {
           BeginRoundPass();
         } else {
           SkipHoldingPriorityNow($currentPlayer);
