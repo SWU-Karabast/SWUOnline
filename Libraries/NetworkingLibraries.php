a13064ec8917b3d67836092d658163e84ade5a2b
OotTheMonk <nathanandrecoder@gmail.com>
2024-04-04 18:43:19 -0500

    Fix weird end step layer bug

--- Libraries/NetworkingLibraries.php
+++ Libraries/NetworkingLibraries.php
@@ -1017,7 +1017,6 @@ function BeginTurnPass()
 {
   global $mainPlayer, $defPlayer, $decisionQueue;
   ResetCombatChainState(); // The combat chain must be closed prior to the turn ending. The close step is outlined in 7.8 - specifically: CR 2.1 - 7.8.7. Fifth and finally, the Close Step ends, and the Action Phase continues. The Action Phase will always continue after the combat chain is closed - so there is another round of priority windows
-  AddLayer("ENDTURN", $mainPlayer, "-");
   ProcessDecisionQueue("");
 }
 
@@ -1038,8 +1037,7 @@ function PlayerSuppress($player)
 function EndStep()
 {
   global $mainPlayer, $turn;
-  $turn[0] = "END";
-  AddLayer("ENDSTEP", $mainPlayer, "-");
+  FinishTurnPass();
   AuraBeginEndPhaseTriggers();
   BeginEndPhaseEffectTriggers();
   PlayerSuppress(1);
