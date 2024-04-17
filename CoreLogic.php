10c6e5a4b639b2de232fb75a87114893abacbcf1
OotTheMonk <nathanandrecoder@gmail.com>
2024-04-17 18:22:58 -0500

    Tarkintown leader restriction

--- CoreLogic.php
+++ CoreLogic.php
@@ -2590,6 +2590,7 @@ function PlayAbility($cardID, $from, $resourcesPaid, $target = "-", $additionalC
     case "1393827469"://Tarkintown
       AddDecisionQueue("SETDQCONTEXT", $currentPlayer, "Choose a unit to deal 3 damage to");
       AddDecisionQueue("MULTIZONEINDICES", $currentPlayer, "THEIRALLY:damagedOnly=true");
+      AddDecisionQueue("MZFILTER", $currentPlayer, "definedType=Leader");
       AddDecisionQueue("MAYCHOOSEMULTIZONE", $currentPlayer, "<-", 1);
       AddDecisionQueue("MZOP", $currentPlayer, "DEALDAMAGE,3", 1);
       break;
