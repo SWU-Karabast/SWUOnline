16bc6ee87346a1f22ac4306b59c5dcaf9f65e895
OotTheMonk <nathanandrecoder@gmail.com>
2024-04-30 18:30:57 -0500

    Fifth Brother secondary damage is not optional

--- AllyAbilities.php
+++ AllyAbilities.php
@@ -617,7 +617,7 @@ function SpecificAllyAttackAbilities($attackID)
       AddDecisionQueue("SETDQCONTEXT", $mainPlayer, "Choose a unit to deal 1 damage to", 1);
       AddDecisionQueue("MULTIZONEINDICES", $mainPlayer, "MYALLY:arena=Ground&THEIRALLY:arena=Ground", 1);
       AddDecisionQueue("MZFILTER", $mainPlayer, "index=MYALLY-" . $attackerAlly->Index());
-      AddDecisionQueue("MAYCHOOSEMULTIZONE", $mainPlayer, "<-", 1);
+      AddDecisionQueue("CHOOSEMULTIZONE", $mainPlayer, "<-", 1);
       AddDecisionQueue("MZOP", $mainPlayer, "DEALDAMAGE,1", 1);
       break;
     case "6827598372"://Grand Inquisitor
