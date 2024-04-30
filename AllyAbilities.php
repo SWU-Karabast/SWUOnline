f4bc3fb88d4f7b6f02a9ed873feafc24b164a256
OotTheMonk <nathanandrecoder@gmail.com>
2024-04-29 19:17:33 -0500

    Fett's Firespray fix enter play state logic

--- AllyAbilities.php
+++ AllyAbilities.php
@@ -154,7 +154,10 @@ function AllyEntersPlayState($cardID, $player, $from="-")
   {
     case "1785627279": return 2;//Millennium Falcon
     case "4300219753"://Fett's Firespray
-      return 2;
+      $char = &GetPlayerCharacter($player);
+      if(CardTitle($char[CharacterPieces()]) == "Boba Fett" || CardTitle($char[CharacterPieces()]) == "Jango Fett") return 2;
+      if(SearchCount(SearchAlliesForTitle($player, "Boba Fett")) > 0 || SearchCount(SearchAlliesForTitle($player, "Jango Fett")) > 0) return 2;
+      return 1;
     default: return 1;
   }
 }
