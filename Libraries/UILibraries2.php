e2bf8ff7f08b070e4cfebba01d747e330a81cc10
OotTheMonk <nathanandrecoder@gmail.com>
2024-05-25 10:39:57 -0500

    Add some fixes

--- Libraries/UILibraries2.php
+++ Libraries/UILibraries2.php
@@ -3,7 +3,7 @@
 use JetBrains\PhpStorm\Language;
 
 require_once("CoreLibraries.php");
-include "../CardDictionary.php";
+require_once("CardDictionary.php");
 
 $isReactFE = false;
 
@@ -154,6 +154,7 @@ function JSONRenderedCard(
 function Card($cardNumber, $folder, $maxHeight, $action = 0, $showHover = 0, $overlay = 0, $borderColor = 0, $counters = 0, $actionDataOverride = "", $id = "", $rotate = false, $lifeCounters = 0, $defCounters = 0, $atkCounters = -1, $from = "", $controller = 0, $subcardNum = 0)
 {
   global $playerID, $darkMode;
+  $opts = [];
   if (is_array($action)) {
     $opts = $action;
     $action = $opts['action'] ?? 0;
@@ -281,7 +282,7 @@ function Card($cardNumber, $folder, $maxHeight, $action = 0, $showHover = 0, $ov
   }
 
   // Shield Icon Style
-  $shieldCount = is_array($opts['subcards']) ? (array_count_values($opts['subcards'])['8752877738'] ?? 0) : 0;
+  $shieldCount = isset($opts) && isset($opts['subcards']) && is_array($opts['subcards']) ? (array_count_values($opts['subcards'])['8752877738'] ?? 0) : 0;
   if ($shieldCount > 0) {
     for ($i = 0; $i < $shieldCount; $i++) {
       $rv .= "<div style='margin: 0px;
@@ -307,7 +308,7 @@ function Card($cardNumber, $folder, $maxHeight, $action = 0, $showHover = 0, $ov
   }
 
   // Sentinel Icon Style
-  if ($opts['hasSentinel']) {
+  if (isset($opts) && isset($opts['hasSentinel']) && $opts['hasSentinel']) {
     $rv .= "<div style='margin: 0px;
     top: 42px; 
     left: 87px;
@@ -327,7 +328,7 @@ function Card($cardNumber, $folder, $maxHeight, $action = 0, $showHover = 0, $ov
 
   }
   // Damage Counter Style
-  $damaged = $opts['currentHP'] < $opts['maxHP']; 
+  $damaged = isset($opts) && isset($opts['currentHP']) && isset($opts['maxHP']) && $opts['currentHP'] < $opts['maxHP']; 
   if ($damaged) {
     $rv .= "<div style='margin: 0px;
     top: 82px;
@@ -354,7 +355,7 @@ function Card($cardNumber, $folder, $maxHeight, $action = 0, $showHover = 0, $ov
 
 
   //Card HP Style
-  if ($opts['currentHP'] != 0) {
+  if (isset($opts) && isset($opts['currentHP']) && $opts['currentHP'] != 0) {
     $bgImage = "./Images/life_v2.png";
     $right = "-2px";
     $top = "67px";
@@ -370,7 +371,7 @@ function Card($cardNumber, $folder, $maxHeight, $action = 0, $showHover = 0, $ov
   }
 
   //Card Power style
-  if ($opts['currentPower'] != 0) {
+  if (isset($opts) && isset($opts['currentPower']) && $opts['currentPower'] != 0) {
     $bgImage = "./Images/attack_v2.png";
     $left = "-2px";
     $top = "67px";
@@ -386,7 +387,7 @@ function Card($cardNumber, $folder, $maxHeight, $action = 0, $showHover = 0, $ov
   }
 
   // Subcards style
-  if (isset($opts['subcards']) && count($opts['subcards']) > 0) {
+  if (isset($opts) && isset($opts['subcards']) && count($opts['subcards']) > 0) {
     for ($i = 0; $i < count($opts['subcards']); $i++) {
       // Don't render shield subcard
       if ($opts['subcards'][$i] != "8752877738") {
