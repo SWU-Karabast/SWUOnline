4e64babb99bfffe79176e95ab7b27c5c5194d778
OotTheMonk <nathanandrecoder@gmail.com>
2024-05-28 18:31:49 -0500

    Modify style a bit

--- NextTurn4.php
+++ NextTurn4.php
@@ -644,7 +644,7 @@
                   var eventArr = eventsArr[i+1].split("!");
                   //Now do the animation
                   var element = document.getElementById("unique-" + eventArr[0]);
-                  element.innerHTML += "<div style='position:absolute; font-size:36px; top:0px; left:0px; width:100%; height:100%; background-color:rgba(255,0,0,0.5); z-index:1000;'>-" + eventArr[1] + "</div>";
+                  element.innerHTML += "<div style='position:absolute; text-align:center; font-size:36px; top:0px; left:0px; width:100%; height:100%; background-color:rgba(255,0,0,0.5); z-index:1000;'><div style='padding: 25px 0; width:100%; height:100%:'>-" + eventArr[1] + "</div></div>";
                   
                 }
               }
