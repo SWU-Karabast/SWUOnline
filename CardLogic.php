1c44d9c5fe646ff6725bf8a0d12d3ec530cc4bf7
OotTheMonk <nathanandrecoder@gmail.com>
2024-06-08 09:19:36 -0500

    Poe Dameron

--- CardLogic.php
+++ CardLogic.php
@@ -3,22 +3,25 @@
 include "CardDictionary.php";
 include "CoreLogic.php";
 
-function PummelHit($player = -1, $passable = false, $fromDQ = false)
+function PummelHit($player = -1, $passable = false, $fromDQ = false, $context="", $may=false)
 {
   global $defPlayer;
   if($player == -1) $player = $defPlayer;
+  if($context == "") $context = "Choose a card to discard";
   if($fromDQ)
   {
     PrependDecisionQueue("ADDDISCARD", $player, "HAND", 1);
     PrependDecisionQueue("MULTIREMOVEHAND", $player, "-", 1);
-    PrependDecisionQueue("CHOOSEHAND", $player, "<-", 1);
-    PrependDecisionQueue("SETDQCONTEXT", $player, "Choose a card to discard", 1);
+    if($may) PrependDecisionQueue("MAYCHOOSEHAND", $player, "<-", 1);
+    else PrependDecisionQueue("CHOOSEHAND", $player, "<-", 1);
+    PrependDecisionQueue("SETDQCONTEXT", $player, $context, 1);
     PrependDecisionQueue("FINDINDICES", $player, "HAND", ($passable ? 1 : 0));
   }
   else {
     AddDecisionQueue("FINDINDICES", $player, "HAND", ($passable ? 1 : 0));
-    AddDecisionQueue("SETDQCONTEXT", $player, "Choose a card to discard", 1);
-    AddDecisionQueue("CHOOSEHAND", $player, "<-", 1);
+    AddDecisionQueue("SETDQCONTEXT", $player, $context, 1);
+    if($may) AddDecisionQueue("MAYCHOOSEHAND", $player, "<-", 1);
+    else AddDecisionQueue("CHOOSEHAND", $player, "<-", 1);
     AddDecisionQueue("MULTIREMOVEHAND", $player, "-", 1);
     AddDecisionQueue("ADDDISCARD", $player, "HAND", 1);
   }
