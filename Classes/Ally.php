1f4dde4645e37bb8ef70d539a39cb9a6b2f34340
LanceVE <lancevanelzen@gmail.com>
2024-06-12 11:27:15 -0500

    fix fett fett armor

--- Classes/Ally.php
+++ Classes/Ally.php
@@ -113,8 +113,13 @@ class Ally {
       }
       switch($subcards[$i]) {
         case "5738033724"://Boba Fett's Armor
-          if(CardTitle($this->CardID()) == "Boba Fett") $amount -= 2;
-          break;
+          if (CardTitle($this->CardID()) == "Boba Fett") {
+            if ($amount >= 2) {
+                $amount -= 2;
+            } else {
+                $amount = 0;
+            }
+        }
         default: break;
       }
     }
