5c8695997eee863f8f3ec91b61171fccfb6a4198
OotTheMonk <nathanandrecoder@gmail.com>
2024-07-01 06:38:25 -0500

    Arquitens assault cruiser

--- CardDictionary.php
+++ CardDictionary.php
@@ -419,6 +419,7 @@ function HasAmbush($cardID, $player, $index, $from)
     case "6097248635"://4-LOM
     case "9483244696"://Weequay Pirate Gang
     case "2556508706"://Resourceful Pursuers
+    case "1086021299"://Arquitens Assault Cruiser
       return true;
     case "2027289177"://Escort Skiff
       return SearchCount(SearchAllies($player, aspect:"Command")) > 1;
