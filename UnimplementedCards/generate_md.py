b890c831d5101e6063883aeef18f325e8062e06d
AMMayberry1 <ammayberry1@gmail.com>
2024-07-01 13:51:46 -0400

    Updates

--- UnimplementedCards/generate_md.py
+++ UnimplementedCards/generate_md.py
@@ -28,31 +28,48 @@ The images below show cards that are _**not yet**_ implemented. If you are havin
 
     return readme_text
 
-def validate_md(root_dir: Path, image_files: list[Path]):
+def validate_md(root_dir: Path, image_files: list[Path], verbose: bool):
     expected_md_file = generate_md(image_files)
     with open(root_dir / README_FILE_NAME, "r") as f:
         actual_md_file = f.read()
 
+    if verbose:
+        print("\n------EXPECTED MD FILE CONTENTS------")
+        print(expected_md_file)
+        print("\n\n------ACTUAL MD FILE CONTENTS------")
+        print(actual_md_file)
+        print("\n")
+
     if expected_md_file != actual_md_file:
-        print("Unimplemented cards readme file is out of date and does not match the contents of './UnimplementedCards/'. Please run `python ./UnimplementedCards/generate_md.py --generate` to update it.", file=sys.stderr)
+        print("Unimplemented cards readme file is out of date and does not match the contents of the folder './UnimplementedCards/'. Please run `python ./UnimplementedCards/generate_md.py generate` to update it.", file=sys.stderr)
         sys.exit(1)
+    
+    print("Unimplemented cards readme validation passed")
 
 def save_md(save_dir: Path, image_files: list[Path]):
     with open(save_dir / README_FILE_NAME, "w") as f:
         f.write(generate_md(image_files))
+    
+    print(f"Unimplemented cards readme file saved to '{save_dir / README_FILE_NAME}'")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Regenerates or validates the readme.md file displaying the unimplemented card images")
-    arg_group = parser.add_mutually_exclusive_group(required=True)
-    arg_group.add_argument("--generate", action="store_true")
-    arg_group.add_argument("--validate", action="store_true")
+    subparsers = parser.add_subparsers(dest="command")
+
+    # process the 'generate' verb
+    generate_parser = subparsers.add_parser("generate")
+
+    # process the 'validate' verb
+    validate_parser = subparsers.add_parser("validate")
+    validate_parser.add_argument("--verbose", action="store_true", help="Log the contents of the expected and actual md files")
+
     args = parser.parse_args()
 
     # note: this will no longer be valid if this file is moved
     unimplemented_cards_dir = Path(os.path.dirname(os.path.realpath(__file__)))
     image_files = list_image_files(unimplemented_cards_dir)
 
-    if args.generate:
+    if args.command == "generate":
         save_md(unimplemented_cards_dir, image_files)
     else:
-        validate_md(unimplemented_cards_dir, image_files)
+        validate_md(unimplemented_cards_dir, image_files, args.verbose)
