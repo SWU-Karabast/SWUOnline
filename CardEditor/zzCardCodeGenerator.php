a940fea2ef3e6b8013e2329575c6e117ffd39f10
OotTheMonk <nathanandrecoder@gmail.com>
2024-08-01 19:58:30 -0500

    Generate images from import schema file

---
+++ CardEditor/zzCardCodeGenerator.php
@@ -0,0 +1,174 @@
+<?php
+
+  include './zzImageConverter.php';
+  include '../Libraries/Trie.php';
+
+  $schemaFile = "./ImportSchema.txt";
+  $handler = fopen($schemaFile, "r");
+  $rootName = trim(fgets($handler));
+  $cardArrayJson = trim(fgets($handler));
+  $properties = explode(",", fgets($handler));
+  fclose($handler);
+
+  $rootPath = "./" . $rootName;
+  if(!is_dir($rootPath)) mkdir($rootPath, 0755, true);
+  if(!is_dir($rootPath . "/WebpImages")) mkdir($rootPath . "/WebpImages", 0755, true);
+  if(!is_dir($rootPath . "/concat")) mkdir($rootPath . "/concat", 0755, true);
+  if(!is_dir($rootPath . "/crops")) mkdir($rootPath . "/crops", 0755, true);
+
+  for($i=0; $i<count($properties); ++$i) {
+    $properties[$i] = trim($properties[$i]);
+    ${$properties[$i]} = [];
+  }
+
+  //$subtitleTrie = [];
+
+    $jsonUrl = "https://20lore.pro/api/";
+    $curl = curl_init();
+    $headers = array(
+      "Content-Type: application/json",
+    );
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+    curl_setopt($curl, CURLOPT_URL, $jsonUrl);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $apiData = curl_exec($curl);
+    curl_close($curl);
+
+
+    $response = json_decode($apiData);
+    
+    //$meta = $response->meta;
+
+    for ($i = 0; $i < count($response->$cardArrayJson); ++$i)
+    {
+      echo("<BR>");
+      $card = $response->$cardArrayJson[$i];
+      for($j=0; $j<count($properties); ++$j) {
+        array_push(${$properties[$j]}, $card->{$properties[$j]});
+        echo(${$properties[$j]}[count(${$properties[$j]})-1] . " ");
+      }
+      echo("<BR>");
+      
+      $setNumber = $card->setId;
+      if($setNumber < 10) $setNumber = "00" . $setNumber;
+      else if($setNumber < 100) $setNumber = "0" . $setNumber;
+      $cardId = $card->setCardId;
+      if($cardId < 10) $cardId = "00" . $cardId;
+      else if($cardId < 100) $cardId = "0" . $cardId;
+      $cardID = $setNumber . "-" . $cardId;
+
+      $imageUrl = "https://20lore.pro/images/" . $cardID . ".webp";
+      echo($imageUrl . "<BR>");
+
+      //$imageUrl = "https://swudb.com/cards/" . $set . "/" . $cardNumber . ".png";
+
+      CheckImage($cardID, $imageUrl, "", "", rootPath:"./" . $rootName . "/");
+
+      continue;
+      if($card->artBack->data != null) {
+        $type2 = $card->type2->data == null ? "" : $card->type2->data->attributes->name;
+        if($type2 == "Leader Unit") $definedType = "Unit";
+        $imageUrl = $card->artBack->data->attributes->formats->card->url;
+        $arr = explode("_", $imageUrl);
+        $arr = explode(".", $arr[count($arr)-1]);
+        $uuid = $arr[0];
+        CheckImage($uuid, $imageUrl, $definedType, isBack:true, set:$set);
+        AddToTries($cardID, $uuid);
+      }
+    }
+
+exit;
+  if (!is_dir("./GeneratedCode")) mkdir("./GeneratedCode", 777, true);
+
+  $generateFilename = "./GeneratedCode/GeneratedCardDictionaries.php";
+  $handler = fopen($generateFilename, "w");
+
+  fwrite($handler, "<?php\r\n");
+
+  GenerateFunction($titleTrie, $handler, "CardTitle", true, "");
+  GenerateFunction($subtitleTrie, $handler, "CardSubtitle", true, "");
+  GenerateFunction($costTrie, $handler, "CardCost", false, -1);
+  GenerateFunction($hpTrie, $handler, "CardHPDictionary", false, -1);
+  GenerateFunction($powerTrie, $handler, "CardPower", false, -1);
+  GenerateFunction($aspectsTrie, $handler, "CardAspects", true, "");
+  GenerateFunction($traitsTrie, $handler, "CardTraits", true, "");
+  GenerateFunction($arenasTrie, $handler, "CardArenas", true, "");
+  GenerateFunction($typeTrie, $handler, "DefinedCardType", true, "");
+  GenerateFunction($type2Trie, $handler, "DefinedCardType2", true, "");
+  GenerateFunction($uniqueTrie, $handler, "CardIsUnique", false, 0);
+  GenerateFunction($hasPlayTrie, $handler, "HasWhenPlayed", false, "false", 1);
+  GenerateFunction($setTrie, $handler, "CardSet", true, "");
+  GenerateFunction($uuidLookupTrie, $handler, "UUIDLookup", true, "");
+
+  fwrite($handler, "?>");
+
+  fclose($handler);
+
+
+  $generateFilename = "./GeneratedCode/GeneratedCardDictionaries.js";
+  $handler = fopen($generateFilename, "w");
+  GenerateFunction($titleTrie, $handler, "CardTitle", true, "", language:"js");
+  fclose($handler);
+
+  function GenerateFunction($cardArray, $handler, $functionName, $isString, $defaultValue, $dataType = 0, $language = "PHP")
+  {
+    if($language == "PHP") fwrite($handler, "function " . $functionName . "(\$cardID) {\r\n");
+    else if($language = "js") fwrite($handler, "function " . $functionName . "(cardID) {\r\n");
+    TraverseTrie($cardArray, "", $handler, $isString, $defaultValue, $dataType, $language);
+    fwrite($handler, "}\r\n\r\n");
+  }
+
+  function AddToTries($cardID, $uuid)
+  {
+    global $uuidLookupTrie, $titleTrie, $subtitleTrie, $costTrie, $hpTrie, $powerTrie, $typeTrie, $type2Trie, $uniqueTrie, $card;
+    global $aspectsTrie, $traitsTrie, $arenasTrie, $hasPlayTrie, $setTrie;
+    if($uuid != "8752877738" && $uuid != "2007868442") {
+      AddToTrie($uuidLookupTrie, $cardID, 0, $uuid);
+    }
+    AddToTrie($titleTrie, $uuid, 0, str_replace('"', "'", $card->title));
+    AddToTrie($subtitleTrie, $uuid, 0, str_replace('"', "'", $card->subtitle));
+    AddToTrie($costTrie, $uuid, 0, $card->cost);
+    AddToTrie($hpTrie, $uuid, 0, $card->hp);
+    AddToTrie($powerTrie, $uuid, 0, $card->power);
+    AddToTrie($typeTrie, $uuid, 0, $card->type->data->attributes->name);
+    AddToTrie($setTrie, $uuid, 0, $card->expansion->data->attributes->code);
+    if($card->type2->data != null) {
+      $type2 = $card->type2->data->attributes->name;
+      if($type2 == "Leader Unit") $type2 = "Unit";
+      AddToTrie($type2Trie, $uuid, 0, $type2);
+    }
+    AddToTrie($uniqueTrie, $uuid, 0, $card->unique == "true" ? 1 : 0);
+    if(str_contains($card->text, "When Played") || str_contains($card->text, "When played")) AddToTrie($hasPlayTrie, $uuid, 0, true);
+    
+    $aspects = "";
+    for($j = 0; $j < count($card->aspects->data); ++$j)
+    {
+      if($aspects != "") $aspects .= ",";
+      $aspects .= $card->aspects->data[$j]->attributes->name;
+    }
+    for($j = 0; $j < count($card->aspectDuplicates->data); ++$j)
+    {
+      if($aspects != "") $aspects .= ",";
+      $aspects .= $card->aspectDuplicates->data[$j]->attributes->name;
+    }
+    AddToTrie($aspectsTrie, $uuid, 0, $aspects);
+
+    $traits = "";
+    for($j = 0; $j < count($card->traits->data); ++$j)
+    {
+      if($traits != "") $traits .= ",";
+      $traits .= $card->traits->data[$j]->attributes->name;
+    }
+    AddToTrie($traitsTrie, $uuid, 0, $traits);
+
+    $arenas = "";
+    for($j = 0; $j < count($card->arenas->data); ++$j)
+    {
+      if($arenas != "") $arenas .= ",";
+      $arenas .= $card->arenas->data[$j]->attributes->name;
+    }
+    AddToTrie($arenasTrie, $uuid, 0, $arenas);
+  }
+
+?>
---
+++ CardEditor/zzImageConverter.php
@@ -0,0 +1,111 @@
+<?php
+
+//TODO: Make the card image generation size customizable in a schema file
+function CheckImage($cardID, $url, $definedType, $isBack=false, $set="SOR", $rootPath="")
+{
+  $filename = $rootPath . "WebpImages/" . $cardID . ".webp";
+  $filenameNew = "UnimplementedCards/" . $cardID . ".webp";
+  $concatFilename = $rootPath . "concat/" . $cardID . ".webp";
+  $cropFilename = $rootPath . "crops/" . $cardID . "_cropped.png";
+  $isNew = false;
+  if(!file_exists($filename))
+  {
+    $imageURL = $url;
+    echo("Image for " . $cardID . " does not exist.<BR>");
+    $handler = fopen($filename, "w");
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $imageURL);
+    curl_setopt($ch, CURLOPT_FILE, $handler);
+    curl_exec($ch);
+    curl_close($ch);
+    //if(filesize($filename) < 10000) { unlink($filename); return; }
+    if(file_exists($filename)) echo("Image for " . $cardID . " successfully retrieved.<BR>");
+    if(file_exists($filename))
+    {
+      echo("Normalizing file size for " . $cardID . ".<BR>");
+      echo("Defined Type: " . $definedType . "<BR>");
+      $image = imagecreatefromwebp($filename);
+      //$image = imagecreatefrompng($filename);
+      //$image = imagecreatefromjpeg($filename);
+      if($definedType == "Base" || $definedType == "Leader") {
+        if(imagesy($image) > imagesx($image)) $image = imagerotate($image, -90, 0);
+        $image = imagescale($image, 628, 450);
+      }
+      else $image = imagescale($image, 450, 628);
+      imagewebp($image, $filename);
+      // Free up memory
+      imagedestroy($image);
+    }
+    $isNew = true;
+  }
+  if($isNew && !file_exists($filenameNew)) {
+    echo("Converting image for " . $cardID . " to new format.<BR>");
+    try {
+      $image = imagecreatefromwebp($filename);
+    } catch(Exception $e) {
+      $image = imagecreatefrompng($filename);
+    }
+    imagewebp($image, $filenameNew);
+    imagedestroy($image);
+  }
+  if(!file_exists($concatFilename))
+  {
+    echo("Concat image for " . $cardID . " does not exist. Converting: $filename<BR>");
+    if(file_exists($filename))
+    {
+      echo("Attempting to convert image for " . $cardID . " to concat.<BR>");
+      
+      $image = imagecreatefromwebp($filename);
+      //$image = imagecreatefrompng($filename);
+      
+      if($definedType == "Event") {
+        $imageTop = imagecrop($image, ['x' => 0, 'y' => 0, 'width' => 450, 'height' => 110]);
+        $imageBottom = imagecrop($image, ['x' => 0, 'y' => 320, 'width' => 450, 'height' => 628]);
+  
+        $dest = imagecreatetruecolor(450, 450);
+        imagecopy($dest, $imageTop, 0, 0, 0, 0, 450, 110);
+        imagecopy($dest, $imageBottom, 0, 111, 0, 0, 450, 404);
+      }
+      else {
+        //$imageTop = imagecrop($image, ['x' => 0, 'y' => 0, 'width' => 450, 'height' => 372]);
+        //$imageBottom = imagecrop($image, ['x' => 0, 'y' => 570, 'width' => 450, 'height' => 628]);
+        $imageTop = imagecrop($image, ['x' => 0, 'y' => 0, 'width' => 450, 'height' => 397]);
+        $imageBottom = imagecrop($image, ['x' => 0, 'y' => 595, 'width' => 450, 'height' => 628]);
+  
+        $dest = imagecreatetruecolor(450, 450);
+        imagecopy($dest, $imageTop, 0, 0, 0, 0, 450, 397);
+        imagecopy($dest, $imageBottom, 0, 398, 0, 0, 450, 53);
+      }
+
+      imagewebp($dest, $concatFilename);
+      // Free up memory
+      imagedestroy($image);
+      imagedestroy($dest);
+      imagedestroy($imageTop);
+      imagedestroy($imageBottom);
+      if(file_exists($concatFilename)) echo("Image for " . $cardID . " successfully converted to concat.<BR>");
+    }
+  }
+  if(!file_exists($cropFilename))
+  {
+    echo("Crop image for " . $cardID . " does not exist.<BR>");
+    if(file_exists($filename))
+    {
+      echo("Attempting to convert image for " . $cardID . " to crops.<BR>");
+      try {
+        $image = imagecreatefromwebp($filename);
+      } catch(Exception $e) {
+        $image = imagecreatefrompng($filename);
+      }
+      //$image = imagecreatefrompng($filename);
+      if($definedType == "Event") $image = imagecrop($image, ['x' => 50, 'y' => 326, 'width' => 350, 'height' => 246]);
+      else $image = imagecrop($image, ['x' => 50, 'y' => 100, 'width' => 350, 'height' => 270]);
+      imagepng($image, $cropFilename);
+      imagedestroy($image);
+      if(file_exists($cropFilename)) echo("Image for " . $cardID . " successfully converted to crops.<BR>");
+    }
+  }
+}
+
+
+?>
