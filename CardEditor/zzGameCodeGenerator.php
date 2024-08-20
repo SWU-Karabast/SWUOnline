04f18c8c745791453d0b3118f42d4e0f7c77a4ad
OotTheMonk <nathanandrecoder@gmail.com>
2024-08-19 20:22:53 -0500

    More code generation

--- CardEditor/zzGameCodeGenerator.php
+++ CardEditor/zzGameCodeGenerator.php
@@ -63,18 +63,58 @@
   fwrite($handler, "?>");
   fclose($handler);
 
+  //Write the class file
+  $filename = $rootPath . "/ZoneClasses.php";
+  $handler = fopen($filename, "w");
+  fwrite($handler, "<?php\r\n");
+  for($i=0; $i<count($zones); ++$i) {
+    $zone = $zones[$i];
+    $zoneName = $zone->Name;
+    fwrite($handler, "class " . $zoneName . " {\r\n");
+    for($j=0; $j<count($zone->Properties); ++$j) {
+      $property = $zone->Properties[$j];
+      fwrite($handler, "  public \$" . $property->Name . ";\r\n");
+    }
+    fwrite($handler, "  function __construct(\$line) {\r\n");
+    fwrite($handler, "    \$arr = explode(\" \", \$line);\r\n");
+    for($j=0; $j<count($zone->Properties); ++$j) {
+      $property = $zone->Properties[$j];
+      $propertyName = $property->Name;
+      $propertyType = $property->Type;
+      fwrite($handler, "    \$this->" . $propertyName . " = ");
+      if($propertyType == "int" || $propertyType == "number") fwrite($handler, "intval(\$arr[" . $j . "]);\r\n");
+      else if($propertyType == "float") fwrite($handler, "floatval(\$arr[" . $j . "]);\r\n");
+      else fwrite($handler, "\$arr[" . $j . "];\r\n");
+    }
+    fwrite($handler, "  }\r\n");
+    fwrite($handler, "}\r\n\r\n");
+  }
+  fwrite($handler, "?>");
+
   //Write the Gamestate parsing file
   $filename = $rootPath . "/GamestateParser.php";
   $handler = fopen($filename, "w");
   fwrite($handler, "<?php\r\n");
+  //Initialize gamestate function
+  fwrite($handler, "function InitializeGamestate() {\r\n");
+  fwrite($handler, GetZoneGlobals($zones) . "\r\n");
+  for($i=0; $i<count($zones); ++$i) {
+    $zone = $zones[$i];
+    $zoneName = $zone->Name;
+    fwrite($handler, "  \$p1" . $zoneName . " = [];\r\n");
+    fwrite($handler, "  \$p2" . $zoneName . " = [];\r\n");
+  }
+  fwrite($handler, "}\r\n\r\n");
   //Write gamestate function
   fwrite($handler, "function WriteGamestate() {\r\n");
   fwrite($handler, GetZoneGlobals($zones) . "\r\n");
+  fwrite($handler, AddWriteGamestate() . "\r\n");
 
   fwrite($handler, "}\r\n\r\n");
   //Parse gamestate function
   fwrite($handler, "function ParseGamestate() {\r\n");
   fwrite($handler, GetZoneGlobals($zones) . "\r\n");
+  fwrite($handler, AddReadGamestate() . "\r\n");
 
   fwrite($handler, "}\r\n\r\n");
   fwrite($handler, "?>");
@@ -90,5 +130,61 @@
       $zoneGlobals .= "  global \$p1" . $zoneName . ", \$p2" . $zoneName . ";\r\n";
     }
     return $zoneGlobals;
+  }
+
+  function AddReadGamestate() {
+    $readGamestate = "";
+    global $rootPath, $zones;
+    $readGamestate .= "  global \$gameName;\r\n";
+    $readGamestate .= "  \$filename = \"" . $rootPath . "/Games/\$gameName/Gamestate.php\";\r\n";
+    $readGamestate .= "  \$handler = fopen(\$filename, \"r\");\r\n";
+    $readGamestate .= "  while (!feof(\$handler)) {\r\n";
+    for($i=0; $i<count($zones); ++$i) {
+      $zone = $zones[$i];
+      $zoneName = $zone->Name;
+      $readGamestate .= AddReadZone($zoneName, 1);
+      $readGamestate .= AddReadZone($zoneName, 2);
+    }
+    $readGamestate .= "  }\r\n";
+    $readGamestate .= "  fclose(\$handler);\r\n";
+    return $readGamestate;
+  }
+
+  function AddReadZone($zoneName, $player) {
+    global $rootPath;
+    $rv = "";
+    $rv .= "    \$line = fgets(\$handler);\r\n";
+    $rv .= "    if (\$line !== false) {\r\n";
+    $rv .= "      \$num = intval(\$line);\r\n";
+    $rv .= "      for(\$i=0; \$i<\$num; ++\$i) {\r\n";
+    $rv .= "        \$line = fgets(\$handler);\r\n";
+    $rv .= "        if (\$line !== false) {\r\n";
+    $rv .= "          \$obj = new " . $zoneName . "(\$line);\r\n";
+    $rv .= "          array_push(\$p" . $player . $zoneName . ", \$obj);\r\n";
+    $rv .= "        }\r\n";
+    $rv .= "      }\r\n";
+    $rv .= "    }\r\n";
+    return $rv;
+  }
+
+  function AddWriteGamestate() {
+    $writeGamestate = "";
+    global $zones;
+    for($i=0; $i<count($zones); ++$i) {
+      $zone = $zones[$i];
+      $zoneName = $zone->Name;
+      $writeGamestate .= AddWriteZone($zoneName, 1);
+      $writeGamestate .= AddWriteZone($zoneName, 2);
+    }
+    return $writeGamestate;
+  }
+
+  function AddWriteZone($zoneName, $player) {
+    $rv = "";
+    $rv .= "  fwrite(\$handler, count(\$p" . $player . $zoneName . ") . \"\\r\\n\");\r\n";
+    $rv .= "  for(\$i=0; \$i<count(\$p" . $player . $zoneName . "); ++\$i) {\r\n";
+    $rv .= "    fwrite(\$handler, \$p" . $player . $zoneName . "[\$i]->Serialize() . \"\\r\\n\");\r\n";
+    $rv .= "  }\r\n";
+    return $rv;
   }
 ?>
