5c54a2205cf2a691b789d9bb4190dc9fefbf81e0
OotTheMonk <nathanandrecoder@gmail.com>
2024-11-23 13:19:13 -0600

    Clone heavy gunner

--- CombatChain.php
+++ CombatChain.php
@@ -111,6 +111,9 @@ function AttackModifier($cardID, $player, $index)
     case "1209133362"://332nd Stalwart
       if(IsCoordinateActive($player)) $modifier += 1;
       break;
+    case "9227411088"://Clone Heavy Gunner
+      if(IsCoordinateActive($player)) $modifier += 2;
+      break;
     case "58f9f2d4a0"://Dr. Aphra
       $discard = &GetDiscard($player);
       $costs = [];
