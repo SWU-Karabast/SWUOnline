e7da1b4cfe5ac94d55272e1ac0d7e104cf739581
Valentin Jubert <valentin.jubert@ideine.fr>
2024-12-03 06:44:55 +0100

    wartime trade official + token does not go in hand or in discard

--- CardSetters.php
+++ CardSetters.php
@@ -167,8 +167,11 @@ function ArsenalTurnFaceUpAbility($cardID, $player)
 function AddHand($player, $cardID)
 {
   $hand = &GetHand($player);
-  $hand[] = $cardID;
-  return count($hand) - 1;
+  // If it's a token, it doesn't go to the hand
+  if($cardID != "3463348370" && $cardID != "3941784506") {
+    $hand[] = $cardID;
+  }
+  return count($hand)-1;
 }
 
 function RemoveResource($player, $index)
