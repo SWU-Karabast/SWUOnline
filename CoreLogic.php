e9c92fa49b6058a26bf3535aac47b4f8b7b6474c
Bruno Braz <bbrazsilveira@gmail.com>
2024-12-04 15:15:41 -0300

    fix(bounty posting): ensure deck is shuffled after search

--- CoreLogic.php
+++ CoreLogic.php
@@ -4310,6 +4310,7 @@ function PlayAbility($cardID, $from, $resourcesPaid, $target = "-", $additionalC
       break;
     case "6151970296"://Bounty Posting
       MZMoveCard($currentPlayer, "MYDECK:trait=Bounty", "MYHAND", isReveal:true, may:true, context:"Choose a bounty to add to your hand");
+      AddDecisionQueue("SHUFFLEDECK", $currentPlayer, "-");
       AddDecisionQueue("YESNO", $currentPlayer, "if you want to play the upgrade", 1);
       AddDecisionQueue("NOPASS", $currentPlayer, "-", 1);
       AddDecisionQueue("FINDINDICES", $currentPlayer, "MZLASTHAND", 1);
