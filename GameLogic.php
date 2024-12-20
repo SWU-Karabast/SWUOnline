2d43386831605be42959fb0d90b1ff34f11a4d0c
Bruno Braz <bbrazsilveira@gmail.com>
2024-12-20 13:11:25 -0300

    fix(deal damage): resolve errors caused by incorrect player reference

--- GameLogic.php
+++ GameLogic.php
@@ -482,7 +482,7 @@ function DecisionQueueStaticEffect($phase, $player, $parameter, $lastResult)
             $isAttackTarget = GetAttackTarget() == $lastResult;
             $isAttacker = AttackerMZID($player) == $lastResult;
             $ally = new Ally($lastResult);
-            $attackerHasOverwhelm = HasOverwhelm($ally->CardID(), $player, $targetArr[1]);
+            $attackerHasOverwhelm = HasOverwhelm($ally->CardID(), $ally->Controller(), $ally->Index());
             $destroyed = $ally->DealDamage($parameterArr[1],
                 enemyDamage:(count($parameterArr) > 2 && $parameterArr[2] != $targetPlayer),
                 fromUnitEffect: count($parameterArr) > 3 && (bool)$parameterArr[3]);
