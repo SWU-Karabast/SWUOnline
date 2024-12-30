e80b0e61fd62ff5031f8f74ba23eb240336f5f7e
Bruno Braz <bbrazsilveira@gmail.com>
2024-12-30 00:29:54 -0300

    fix: prevent unnecessary triggers

--- AllyAbilities.php
+++ AllyAbilities.php
@@ -1194,7 +1194,7 @@ function AllyHasPlayCardAbility($playedCardID, $playedCardUniqueID, $from, $card
       case "9850906885"://Maz Kanata
         return !$thisIsNewlyPlayedAlly && DefinedTypesContains($playedCardID, "Unit");
       case "3952758746"://Toro Calican
-        return !$thisIsNewlyPlayedAlly && TraitContains($playedCardID, "Bounty Hunter", $player);
+        return !$thisIsNewlyPlayedAlly && !$thisAlly->IsExhausted() && TraitContains($playedCardID, "Bounty Hunter", $player);
       case "724979d608"://Cad Bane Leader Unit
       case "0981852103"://Lady Proxima
         return !$thisIsNewlyPlayedAlly && TraitContains($playedCardID, "Underworld", $player);
@@ -1211,7 +1211,7 @@ function AllyHasPlayCardAbility($playedCardID, $playedCardUniqueID, $from, $card
         return DefinedTypesContains($playedCardID, "Unit");
       case "0142631581"://Mas Amedda
       case "9610332938"://Poggle the Lesser
-        return !$thisIsNewlyPlayedAlly && DefinedTypesContains($playedCardID, "Unit");
+        return !$thisIsNewlyPlayedAlly && !$thisAlly->IsExhausted() && DefinedTypesContains($playedCardID, "Unit");
       case "3589814405"://tactical droid commander
         return !$thisIsNewlyPlayedAlly && DefinedTypesContains($playedCardID, "Unit") && TraitContains($playedCardID, "Separatist", $player);
       default: break;
