35199b27d4411c6d580964ab46e79ff891f6592b
Bruno Braz <bbrazsilveira@gmail.com>
2025-01-09 00:54:54 -0300

    fix(resource): add destroyed resource to the owner's discard pile

--- CardSetters.php
+++ CardSetters.php
@@ -202,12 +202,14 @@ function DestroyArsenal($player, $index=-1)
 {
   $arsenal = &GetArsenal($player);
   $cardIDs = "";
+  $otherPlayer = $player == 1 ? 2 : 1;
   for($i = 0; $i < count($arsenal); $i += ArsenalPieces()) {
     if($index > -1 && $index != $i) continue;
     if($cardIDs != "") $cardIDs .= ",";
     $cardIDs .= $arsenal[$i];
     WriteLog(CardLink($arsenal[$i], $arsenal[$i]) . " was destroyed from the arsenal");
-    AddGraveyard($arsenal[$i], $player, "ARS");
+    $owner = $arsenal[$i + 6] == "-1" ? $player  : $otherPlayer ;
+    AddGraveyard($arsenal[$i], $owner, "ARS");
     for($j=$i+ArsenalPieces()-1; $j>=$i; --$j) unset($arsenal[$j]);
   }
   $arsenal = array_values($arsenal);
