15db0e5db4ae62c591209c38e6d05f7d035310be
Gabriele N. Tornetta <phoenix1987@gmail.com>
2025-01-25 08:44:29 +0000

    fix: name of the player to go first in chat
    This change fixes the name displayed in the chat/log when the player
    that won the dice roll decides whether to go first or second.

--- APIs/ChooseFirstPlayer.php
+++ APIs/ChooseFirstPlayer.php
@@ -40,10 +40,12 @@ if ($authKey != $targetAuth) {
 
 if ($action == "Go First") {
   $firstPlayer = $playerID;
+  $firstPlayerName = $playerName;
 } else {
   $firstPlayer = ($playerID == 1 ? 2 : 1);
+  $firstPlayerName = $otherPlayerName;
 }
-WriteLog(FmtPlayer($playerName, $playerID) . " will go first.", path: "../");
+WriteLog(FmtPlayer($firstPlayerName, $firstPlayer) . " will go first.", path: "../");
 $gameStatus = $MGS_P2Sideboard;
 SetCachePiece($gameName, 14, $gameStatus);
 GamestateUpdated($gameName);
