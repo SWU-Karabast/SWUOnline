54bf3497de8744941743f9c181356e38dbfd9acf
Mario Torres <mariotorresdev@gmail.com>
2025-02-19 16:40:10 -0600

    fix(JTL): typo for iden pilot

--- AllyAbilities.php
+++ AllyAbilities.php
@@ -1336,6 +1336,7 @@ function AllyPlayedAsUpgradeAbility($cardID, $player, $targetAlly) {
       break;
     case "9325037410"://Iden Versio
       $targetAlly->Attach("8752877738");//Shield Token
+      break;
     case "0514089787"://Frisk
       DefeatUpgrade($player, true, upgradeFilter:"maxCost=2");
       break;
