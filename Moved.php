2e9306ed96140412966081246fd685d59083ad2f
Mario Torres <mariotorresdev@gmail.com>
2025-02-27 07:55:56 -0600

    feat(rebrand): added Moved page

---
+++ Moved.php
@@ -0,0 +1,25 @@
+<head>
+  <meta charset="utf-8">
+  <title>Petranaki</title>
+  <link rel="stylesheet" href="./css/chat2.css">
+  <link rel="stylesheet" href="./css/karabast011625.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Gemunu+Libre:wght@200..800&display=swap" rel="stylesheet">
+</head>
+<body>
+  <div style="height:600px; width: 800px; padding: 32px; position:relative; top:10%; left: 25%; background-color: rgba(100,100,100,0.7);">
+    <p style="font-size: 80px; font-weight: 700;">We've Moved!</p>
+    <p style="font-size: 28px;">
+      The Karabast team is getting ready to release the new engine under public beta. To play using the previous engine, please head on over to <a href="https://petranaki.net">Petranaki.net</a>!
+    </p>
+    <p style="font-size: 28px;">
+      There, you will still be able to test out your decks and strategies, preview the new set, and play with your friends. We hope to see you there!
+    </p>
+    <br/><br/><br/>
+    <p style="font-size: 18px;">Be sure to check out the <a href="https://discord.gg/hKRaqHND4v">Karabast</a> Discord for updates and announcements on the new engine.</p>
+    <p style="font-size: 18px;">For the Petranaki Discord, please visit <a href="https://discord.gg/AN5GEXSu">here</a>.</p>
+  </div>
+</body>
+
