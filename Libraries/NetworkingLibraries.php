dd5e8d6b8361b1530ef75cc6e47f15e593bfbd3c
Mario Torres <mariotorresdev@gmail.com>
2025-03-02 19:57:45 -0600

    fix(JTL): fixed Arquitens for token targets

--- Libraries/NetworkingLibraries.php
+++ Libraries/NetworkingLibraries.php
@@ -1006,7 +1006,7 @@ function ResolveMultiTarget(Ally $attacker, $mainPlayer, $defPlayer)
 function ResolveSingleTarget($mainPlayer, $defPlayer, $target, $attackerPrefix, Ally $attacker, $totalAttack, $totalDefense)
 {
   global $combatChain, $combatChainState, $mainPlayer, $defPlayer, $CCS_CombatDamageReplaced;
-  global $CCS_DamageDealt;
+  global $CCS_DamageDealt, $CCS_CachedLastDestroyed;
 
   $targetArr = explode("-", $target);
   $attackerID = $attacker->CardID();
@@ -1021,7 +1021,8 @@ function ResolveSingleTarget($mainPlayer, $defPlayer, $target, $attackerPrefix, 
       BlizzardAssaultATAT($mainPlayer, $totalAttack);
     }
     if ($attackerID == "1086021299") {
-      ArquitensAssaultCruiser($mainPlayer);
+      $lastDestroyed = explode(";",$combatChainState[$CCS_CachedLastDestroyed]);
+      ArquitensAssaultCruiser($mainPlayer, $lastDestroyed[0]);
     }
     ClearAttackTarget();
     CompletesAttackEffect($attackerID);
@@ -1040,6 +1041,7 @@ function ResolveSingleTarget($mainPlayer, $defPlayer, $target, $attackerPrefix, 
     $shootsFirst = ShouldCombatDamageFirst();
     $preventDamage = ShouldPreventCombatDamage();
     $defenderPower = $defender->CurrentPower();
+    $defenderCardID = $defender->CardID();
     if ($defenderPower < 0)
       $defenderPower = 0;
     $excess = $totalAttack - $defender->Health();
@@ -1071,7 +1073,7 @@ function ResolveSingleTarget($mainPlayer, $defPlayer, $target, $attackerPrefix, 
     CompletesAttackEffect($attackerID);
   }
   if ($attackerID == "1086021299") {
-    ArquitensAssaultCruiser($mainPlayer);
+    ArquitensAssaultCruiser($mainPlayer, $defenderCardID);
   }
   ProcessDecisionQueue();
 }
@@ -2271,8 +2273,7 @@ function IsUnitException($cardID) {
   };
 }
 
-function RelentlessLostAbilities($player): bool
-{
+function RelentlessLostAbilities($player): bool {
   $relentlessIndex = SearchAlliesForCard($player, "3401690666");
   if ($relentlessIndex != "") {
     $ally = new Ally("MYALLY-" . $relentlessIndex, $player);
@@ -2281,20 +2282,25 @@ function RelentlessLostAbilities($player): bool
   return true;
 }
 
-function BlizzardAssaultATAT($player, $excess)
-{
+function BlizzardAssaultATAT($player, $excess) {
   AddDecisionQueue("SETDQCONTEXT", $player, "Choose a unit to deal " . $excess . " damage to");
   AddDecisionQueue("MULTIZONEINDICES", $player, "THEIRALLY:arena=Ground");
   AddDecisionQueue("MAYCHOOSEMULTIZONE", $player, "<-", 1);
   AddDecisionQueue("MZOP", $player, "DEALDAMAGE,$excess,$player,1", 1);
 }
 
-function ArquitensAssaultCruiser($player)
-{
+function ArquitensAssaultCruiser($player, $cardID) {
   $defPlayer = $player == 1 ? 2 : 1;
-  $discard = &GetDiscard($defPlayer);
-  $defeatedCard = RemoveDiscard($defPlayer, count($discard) - DiscardPieces());
-  AddResources($defeatedCard, $player, "PLAY", "DOWN", isExhausted: true);
+  if(!IsToken($cardID)) {
+    $discard = GetDiscard($defPlayer);
+    for($i=0; $i<count($discard); $i+=DiscardPieces()) {
+      if($discard[$i] == $cardID) {
+        AddResources($cardID, $player, "PLAY", "DOWN", isExhausted: true);
+        RemoveDiscard($defPlayer, $i);
+        break;
+      }
+    }
+  }
 }
 
 function LayersContainAnyWhenPlayAbilitiesForPlayer($player) {
