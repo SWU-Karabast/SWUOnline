b1aae797bb3cdc73084d189c6b43b0b5c9d8206c
Bruno Braz <bbrazsilveira@gmail.com>
2025-03-12 00:17:19 -0300

    chore(home): UI tweaks

--- Disclaimer.php
+++ Disclaimer.php
@@ -1,3 +1,3 @@
 <div class="disclaimer">
-  <p class="bg-yellow">Petranaki is in no way affiliated with Disney or Fantasy Flight Games. Star Wars characters, cards, logos, and art are property of Disney and/or Fantasy Flight Games.</p>
+  <p>Petranaki is in no way affiliated with Disney or Fantasy Flight Games. Star Wars characters, cards, logos, and art are property of Disney and/or Fantasy Flight Games.</p>
 </div>
